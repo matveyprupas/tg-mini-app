2d54f8fd1b50ade3e84badb37a6dcf394d4d698a
Matvey Prupas <matvey.prupas@ventionteams.com>
2024-08-06 21:26:30 +0300

    feat: add item into cloud storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 
 
+// Сделать телеграм веб апп, который:
+
+// - будет работать с телеграм cloud storage и записывать туда количество визитов пользователя.
+// - в зависимости от локали пользователя будет выдавать приветствие на русском или английском языке, обращаясь к пользователю по имени и сообщая сколько раз пользователь уже тут был
+// - при попытке дебажить - очищать адресную строку и выдавать сообщение, что нельзя подсматривать
+
 function App() {
-  const {tg} = useTelegram();
-  
+  const {tg, setVisitCount, getVisitCount} = useTelegram();
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const count = await getVisitCount();
+      setCount(count);
+    })();
+  });
+
   const handleClick = () =>  {
-    console.log('handleClick', tg)
+    console.log('handleClick', tg);
+    console.log('handleClick', setVisitCount());
   }
   return (
     <div className="App">
-      <header>Hello {tg.initDataUnsafe.user.first_name}</header>
+      <header>
+        <h1>
+          Hello, {tg.initDataUnsafe.user.first_name}
+        </h1>
+      </header>
+
+      <p>You have been there {count} times!</p>
       <button onClick={handleClick}>Button</button>
     </div>
   );
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,7 +1,35 @@
 const tg = window.Telegram.WebApp;
 
 export const useTelegram = () => {
+
+  const getVisitCount = async () => {
+    console.log('getVisitCount start');
+    let res = null;
+
+    tg.CloudStorage.getItem('visitCount', (error, value) => {
+      console.log('getVisitCount callback start');
+      if(error) throw error;
+
+      console.log('getVisitCount callback end');
+      res = value;
+    });
+
+    console.log('getVisitCount end');
+    return res;
+  }
+
+  const setVisitCount = async () => {
+    console.log('setVisitCount start');
+
+    const count = await getVisitCount();
+    tg.CloudStorage.setItem('visitCount', count + 1);
+
+    console.log('setVisitCount end count', count);
+  }
+
   return ({
-    tg
+    tg,
+    getVisitCount,
+    setVisitCount
   })
 }
