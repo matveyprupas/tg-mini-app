ae2c1c32fc694651fe7879de93910675d70b5983
Matvey Prupas <matvey.prupas@ventionteams.com>
2024-08-07 15:38:17 +0300

    fix: remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useCloudStorage, useInitData } from '@vkruglikov/react-telegram-web-app';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Greeting } from './components/Greeting/Greeting';
 
 function App() {
   const [initDataUnsafe] = useInitData();
   const currentQueryId = initDataUnsafe?.query_id;
-  const {getKeys, getItems, getItem, setItem} = useCloudStorage();
-  const [keys, setKeys] = useState<string[]>([]);
-  const [items, setItems] = useState<string[]>([]);
-  const [visits, setVisits] = useState<number>(0);
+  const {getItem, setItem} = useCloudStorage();
+  const [visits, setVisits] = useState<number | null>(null);
 
   useEffect(() => {
     if(!currentQueryId) return
@@ -40,27 +38,12 @@ function App() {
     })
   }, [currentQueryId, getItem, setItem]);
 
-  useEffect(() => {
-    getKeys().then(keys => {
-      setKeys(keys)
-      getItems(keys).then(items => setItems(items));
-    });
-  }, [getKeys, getItems]);
-
-  console.log('App render: ', keys, items);
-
   return (
     <div className="App flex flex-col gap-4 items-center">
       <header className="App-header font-bold">
         <Greeting>{initDataUnsafe?.user?.first_name}</Greeting>
       </header>
       <p>You have been there ${visits} times!</p>
-
-      <h3>Keys</h3>
-      {keys.length && keys.map(el => <p key={el}>{el}</p>)}
-      
-      {/* <h3>Items</h3>
-      {items.length && items.map(el => <p key={el}>{el}</p>)} */}
     </div>
   );
 }
