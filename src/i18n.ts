8cdeb05d66fee50b40fac29060cf1aee4588e2e7
Matvey Prupas <matvey.prupas@ventionteams.com>
2024-08-07 19:26:52 +0300

    fix: add log

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// import Backend from 'i18next-http-backend';
-// import LanguageDetector from 'i18next-browser-languagedetector';
 import enTranslation from './locales/en/translate.json'
 import ruTranslation from './locales/ru/translate.json'
 
@@ -15,17 +13,12 @@ const resources = {
   }
 };
 
+console.log(resources);
+
 i18n
-  // .use(Backend)
-  // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    // backendOptions: [
-    //   {
-    //     loadPath: '/locales/{{lng}}/{{ns}}.json',
-    //   },
-    // ],
     fallbackLng: 'en',
     debug: true,
 
