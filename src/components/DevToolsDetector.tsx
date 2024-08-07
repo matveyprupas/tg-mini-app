0930f1b72db38f698ee4253d9177e7cd0d13aefe
Matvey Prupas <matvey.prupas@ventionteams.com>
2024-08-07 22:08:08 +0300

    fix: change detect logic

--- src/components/DevToolsDetector.tsx
+++ src/components/DevToolsDetector.tsx
@@ -1,19 +1,27 @@
-import { addListener } from 'devtools-detector';
-import { useState } from 'react';
+import { addListener, launch } from 'devtools-detector';
+import { useEffect, useState } from 'react';
 
 
 export const DevToolsDetector = () => {
   const [text, setText] = useState('');
 
-  addListener(
-    (isOpen) => {
-      const text = isOpen ? 'open' : 'close';
-      setText(text)
-    }
-  );
+  
+  useEffect(() => {
+    addListener(
+      (isOpen) => {
+        const text = isOpen ? 'open' : 'close';
+        setText(text)
+      }
+    );
+    launch();
+  })
+
+  console.log(text);
   
   return (
     <div className="">
+      <p>Static text</p>
+      <p>{text}</p>
       {text}
     </div>
   );
